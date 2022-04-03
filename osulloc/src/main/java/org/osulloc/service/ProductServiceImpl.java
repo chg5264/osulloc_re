9885802e587ab859bfa9e8adbdd9353ce60f6de7
hyesongcom <hyesongcom@LAPTOP-VRC0O8A2>
2022-04-03 14:59:23 +0900

    2022.04.03

---
+++ osulloc/src/main/java/org/osulloc/service/ProductServiceImpl.java
@@ -0,0 +1,48 @@
+package org.osulloc.service;
+
+import java.util.ArrayList;
+
+import org.osulloc.domain.ProductDTO;
+import org.osulloc.domain.ProductReviewDTO;
+import org.osulloc.mapper.ProductMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+	@Autowired
+	private ProductMapper pmapper;
+
+	public ArrayList<ProductDTO> product(ProductDTO prod) {
+		System.out.println("선택상품 = " +pmapper.product(prod) );
+		return pmapper.product(prod);
+	}
+
+	public ArrayList<ProductDTO> product2(ProductDTO prod) {
+		return pmapper.product2(prod);
+	}
+
+	public ArrayList<ProductDTO> product3se(ProductDTO prod) {
+		return pmapper.product3se(prod);
+	}
+
+	public void product3in(ProductDTO prod) {
+		pmapper.product3in(prod);
+	}
+
+	// 제품 상세페이지 select
+	public ProductDTO productse(ProductDTO prod) {
+
+		return pmapper.productse(prod);
+
+	}
+
+	public void addProduct(ProductDTO product) {
+		System.out.println("service=" + product);
+
+		pmapper.addProduct(product);
+
+	}
+
+}
